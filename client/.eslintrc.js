afc8151ca455de6f0673afd93897ce3a8f458b71
Tam Le <philosocode@gmail.com>
2019-03-27 21:04:56 -0600

    Setup ESLint; still doesn't play nicely with Prettier

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,23 +1,17 @@
+// Extend react-script's version of ESLint: 
+// http://rahulgaba.com/front-end/2019/02/17/Use-custom-eslint-config-in-create-react-app-using-three-simple-steps-No-external-dependencies.html
+
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: 'airbnb',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react'
+  "extends": [
+    "react-app",
+    "airbnb"
   ],
-  rules: {
-  },
-};
+  "rules" : {
+    // "indent": ["error", 2],
+    "comma-dangle": ["error", "never"],
+    "react/destructuring-assignment": 0,
+    "react/jsx-filename-extension": 0
+
+    // more custom rules here
+  }
+}
