f44eb0e4b67ac9b2875f36be9bf73064b660c901
Tam Le <philosocode@gmail.com>
2019-04-22 22:33:53 -0600

    Tweak History and header navbar styles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,24 +4,34 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <div id="toggle">
     <input className="checkbox" type="checkbox" />
-    <span></span>
-    <span></span>
-    <span></span>
+    <span />
+    <span />
+    <span />
     <ul className="navbar" id="navbar">
       <li className="navbar__item">
-        <NavLink to="/" className="navbar__link" exact activeClassName="navbar__link--active"><button className="navbutton">Landing</button></NavLink>
+        <NavLink to="/" className="navbar__link" exact activeClassName="navbar__link--active">
+          Landing
+        </NavLink>
       </li>
       <li className="navbar__item">
-        <NavLink to="/login" className="navbar__link" exact activeClassName="navbar__link--active"><button className="navbutton">Login</button></NavLink>
+        <NavLink to="/login" className="navbar__link" exact activeClassName="navbar__link--active">
+          Login
+        </NavLink>
       </li>
       <li className="navbar__item">
-        <NavLink to="/register" className="navbar__link" activeClassName="navbar__link--active"><button className="navbutton">Register</button></NavLink>
+        <NavLink to="/register" className="navbar__link" activeClassName="navbar__link--active">
+          Register
+        </NavLink>
       </li>
       <li className="navbar__item">
-        <NavLink to="/dashboard" className="navbar__link" activeClassName="navbar__link--active"><button className="navbutton">Dashboard</button></NavLink>
+        <NavLink to="/dashboard" className="navbar__link" activeClassName="navbar__link--active">
+          Dashboard
+        </NavLink>
       </li>
       <li className="navbar__item">
-        <NavLink to="/history" className="navbar__link" activeClassName="navbar__link--active"><button className="navbutton">History</button></NavLink>
+        <NavLink to="/history" className="navbar__link" activeClassName="navbar__link--active">
+          History
+        </NavLink>
       </li>
     </ul>
   </div>
