59baf79c42b821483a7ff8ff19d95ae5f632bdaf
Tam Le <philosocode@gmail.com>
2019-04-23 22:06:26 -0600

    Add conditional navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,40 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Navbar = () => (
-  <div id="toggle">
-    <input className="checkbox" type="checkbox" />
-    <span />
-    <span />
-    <span />
-    <ul className="navbar" id="navbar">
-      <li className="navbar__item">
-        <NavLink to="/" className="navbar__link" exact activeClassName="navbar__link--active">
-          Landing
-        </NavLink>
-      </li>
-      <li className="navbar__item">
-        <NavLink to="/login" className="navbar__link" exact activeClassName="navbar__link--active">
-          Login
-        </NavLink>
-      </li>
-      <li className="navbar__item">
-        <NavLink to="/register" className="navbar__link" activeClassName="navbar__link--active">
-          Register
-        </NavLink>
-      </li>
-      <li className="navbar__item">
-        <NavLink to="/dashboard" className="navbar__link" activeClassName="navbar__link--active">
-          Dashboard
-        </NavLink>
-      </li>
-      <li className="navbar__item">
-        <NavLink to="/history" className="navbar__link" activeClassName="navbar__link--active">
-          History
-        </NavLink>
-      </li>
-    </ul>
-  </div>
-);
+import { logoutUser } from '../../actions/authActions';
 
-export default Navbar;
+class Navbar extends Component {
+  handleLogout = (evt) => {
+    evt.preventDefault();
+    this.props.logoutUser();
+  }
+
+  render() {
+    const isLoggedIn = this.props.auth.username !== '';
+    let dynamicNavbar;
+
+    if (isLoggedIn) {
+      dynamicNavbar = (
+        <ul className="navbar" id="navbar">
+          <li className="navbar__item">
+            <NavLink to="/dashboard" className="navbar__link" activeClassName="navbar__link--active">
+              Dashboard
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink to="/history" className="navbar__link" activeClassName="navbar__link--active">
+              History
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <a href="/logout" onClick={this.handleLogout} className="navbar__link">Logout</a>
+          </li>
+        </ul>
+      );
+    } else {
+      dynamicNavbar = (
+        <ul className="navbar" id="navbar">
+          <li className="navbar__item">
+            <NavLink to="/" className="navbar__link" exact activeClassName="navbar__link--active">
+              Landing
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink to="/login" className="navbar__link" exact activeClassName="navbar__link--active">
+              Login
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink to="/register" className="navbar__link" activeClassName="navbar__link--active">
+              Register
+            </NavLink>
+          </li>
+        </ul>
+      );
+    }
+
+    return (
+      <div id="toggle">
+        <input className="checkbox" type="checkbox" />
+        <span />
+        <span />
+        <span />
+        { dynamicNavbar }
+      </div>
+    );
+  }
+}
+
+Navbar.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.shape({
+    username: PropTypes.string,
+    expires: PropTypes.string
+  }).isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser })(Navbar);
