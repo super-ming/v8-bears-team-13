1429c742b1ba2ea3cbda5ad87c26018c47b1d80e
Ming Ho <hkboi108@hotmail.com>
2019-04-26 00:36:54 -0700

    Add chart svg to landing page

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { ReactComponent as Chart } from './chart.svg';
 
 const Landing = () => (
   <>
     <div className="content">
-      <p className="body-text">Get into the habit of saving money effective for the things you love.</p>
-      <p className="body-text">This app will help you manage your revenue and expenses more effectively.</p>
+      <div className="landing-body">
+        <div className="landing-img">
+          <Chart />
+        </div>
+        <div className="landing-text">
+          <p className="body-text">Get into the habit of saving money effective for the things you love.</p>
+          <p className="body-text">This app will help you manage your revenue and expenses more effectively.</p>
+        </div>
+      </div>
     </div>
   </>
 );
