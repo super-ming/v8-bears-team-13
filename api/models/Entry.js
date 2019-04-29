578866acec9d38002c6d0d26114ff8b1ae564099
Ming Ho <hkboi108@hotmail.com>
2019-04-29 16:30:04 -0700

    Add function to get latest entries to Entry model

--- api/models/Entry.js
+++ api/models/Entry.js
@@ -1,22 +1,23 @@
 const moment = require('moment');
 const db = require('../db/config');
+const User = require('./User');
 
-exports.addEntry = (data) => {
-    const {transaction, category, entry, amount, full_date, created_at, recurring} = data;
-    return db.one('INSERT INTO entries (user_id, transact_id, category_id, entry_desc, amount, full_date, created_at, recurring) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
-    [2, transaction, category, entry, amount, full_date, created_at, recurring]);
+exports.addEntry = async (data) => {
+  const { username, transaction, category, entry, amount, full_date, created_at, recurring } = data;
+  const userId = await User.findUserId(username).then(user => user.id);
+  return db.one('INSERT INTO entries (user_id, transact_id, category_id, entry_desc, amount, full_date, created_at, recurring) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
+    [userId, transaction, category, entry, amount, full_date, created_at, recurring]);
 };
 
-exports.getLatestEntries = async (data) => {
-    return await db.task('getLatestEntries', async (t) => {
-        const previousMonthStart = moment().date(0).startOf('month').startOf('day').format('YYYY-MM-DD HH:mm:ss');
-        const user = await t.one('SELECT id from users WHERE username = $1', [data.username]);
-        return db.any('SELECT * from entries WHERE user_id = $1 AND created_at >= $2', [user.id, previousMonthStart])
-            .then(data => {
-                return data;
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    })
+exports.getLatestEntries = async (id) => {
+  return db.task('getLatestEntries', async (t) => {
+    const previousMonthStart = moment().date(0).startOf('month').startOf('day').format('YYYY-MM-DD HH:mm:ss');
+    return db.any('SELECT * from entries JOIN categories on entries.category_id = categories.id WHERE entries.user_id = $1 AND entries.created_at >= $2', [id, previousMonthStart])
+      .then(data => {
+        return data;
+      })
+        .catch(err => {
+        console.error(err);
+      });
+  })
 };
