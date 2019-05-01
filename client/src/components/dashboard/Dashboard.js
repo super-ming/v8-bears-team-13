82aab51fc365032fe842cf5ee6dd58ab8ee4046c
Ming Ho <hkboi108@hotmail.com>
2019-04-30 23:35:44 -0700

    Update dashboard to pass entry details to entry list and entry component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -16,11 +16,6 @@ import formatMoney from '../../helpers/formatMoney';
 class Container extends React.Component {
   constructor(props) {
     super(props);
-    // this.addNewEntry = this.addNewEntry.bind(this);
-    // this.setDashDefault = this.setDashDefault.bind(this);
-    // this.state = {
-    //   status: 'dash'
-    // };
   }
 
   componentDidMount() {
@@ -31,20 +26,16 @@ class Container extends React.Component {
     this.props.getLatestEntries(this.props.auth.userId);
   }
 
-  // statusAdd = () => {
-  //   this.setState({status:'add'});
-  //   console.log(this.state.status);
-  // }
-
   render() {
     console.log(this.props);
+    
     const index = () => {
       const { status, latestEntries } = this.props;
       let income = 0;
       let expense = 0;
       let savings = 0;
       if (latestEntries) {
-        latestEntries.map((entry) => {
+        latestEntries.map(entry => {
           if (!entry.transact_id) {
             income += parseFloat(entry.amount);
           }
@@ -73,7 +64,7 @@ class Container extends React.Component {
             <div className="dash__saved">
               <p>
                 You have saved{' '}
-                <span className="dash__saved--big">${formatMoney(savings)}</span> so far
+                <span className="dash__saved--big">{formatMoney(savings)}</span> so far
                 this month.
               </p>
             </div>
@@ -120,8 +111,8 @@ const mapDispatchToProps = (dispatch) => ({
   addNewEntry: () => {
     dispatch(addEntry());
   },
-  editEntry: () => {
-    dispatch(editEntry());
+  editEntry: (entry) => {
+    dispatch(editEntry(entry));
   },
   setDashDefault: () => {
     dispatch(dashDefault());
