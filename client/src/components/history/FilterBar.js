dbd3d0326461e96fb2e1d59cb2d87f27d583c972
Tam Le <philosocode@gmail.com>
2019-05-04 14:42:20 -0600

    Responsive History. Added a submit button for filtering

--- client/src/components/history/FilterBar.js
+++ client/src/components/history/FilterBar.js
@@ -7,22 +7,25 @@ class FilterBar extends Component {
 
   render() {
     return (
-      <div className="filter-bar">
-        <SearchBar />
+      <>
+        <form className="filter-bar">
+          <SearchBar />
 
-        <select name="type" id="type" className="filter-bar__select">
-          <option value="expense">Expense</option>
-          <option value="income">Income</option>
-        </select>
+          <select name="type" id="type" className="filter-bar__select">
+            <option value="expense">Expense</option>
+            <option value="income">Income</option>
+          </select>
 
-        <select name="date" id="date" className="filter-bar__select">
-          <option value="this-month">This Month</option>
-          <option value="three-months">Last Three Months</option>
-          <option value="six-months">Last Six Months</option>
-          <option value="year">Last Year</option>
-          <option value="all-time">All Time</option>
-        </select>
-      </div>
+          <select name="date" id="date" className="filter-bar__select">
+            <option value="this-month">This Month</option>
+            <option value="three-months">Last Three Months</option>
+            <option value="six-months">Last Six Months</option>
+            <option value="year">Last Year</option>
+            <option value="all-time">All Time</option>
+          </select>
+          <button className="button filter-bar__button" type="submit">Search</button>
+        </form>
+      </>
     );
   }
 }
