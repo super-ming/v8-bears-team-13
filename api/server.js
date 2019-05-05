95047f9d6fb3fc13cccce5308a259e7a912d8436
Tam Le <philosocode@gmail.com>
2019-05-04 21:35:36 -0600

    Prepare to try to build

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require("passport");
@@ -40,6 +41,14 @@ app.use('/api/entries', entryRoutes);
 app.use('/api/history', historyRoutes);
 
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+  });
+}
 
 app.get('/', (req, res) => {
   res.json({ info: 'Node, Express, Postgres API' });
